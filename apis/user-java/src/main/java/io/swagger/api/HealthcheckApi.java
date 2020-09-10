38251a178a4a8720743032bf468cfc1d97365a2d
mortenhbakken <47855505+mortenhbakken@users.noreply.github.com>
2020-09-10 11:32:33 +0200

    Update HealthcheckApi.java
    adding a comment

--- apis/user-java/src/main/java/io/swagger/api/HealthcheckApi.java
+++ apis/user-java/src/main/java/io/swagger/api/HealthcheckApi.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-08-03T19:26:46.543Z")
-
+//random comment
 @Api(value = "healthcheck", description = "the healthcheck API")
 public interface HealthcheckApi {
 
